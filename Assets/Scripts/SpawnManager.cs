7cd92105ee84f8619dca7781245a7a9d22f3cbc4
pppiyo <yueqinli@usc.edu>
2023-09-24 16:34:39 -0700

    Refinement of SpawnManager. Housekeeping.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -22,47 +22,45 @@ public class SpawnManager : MonoBehaviour
     private float FRUIT_LIMIT_DOWN = 60; // horizontal: z: + <- -
 
     // Start is called before the first frame update
-    void Start() { }
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomPrefab", 2, 2);
+    }
 
     // Update is called once per frame
-    void Update()
+    void Update() { }
+
+    void SpawnRandomPrefab()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        // 0: eagle, 1: fruit, 2: pebble, 3: worm
+        int prefabIndex = Random.Range(0, allPrefabs.Length);
+
+        if (prefabIndex == 0) // eagle
         {
-            float x = 0;
-            float y = 0;
-            float z = 0;
-            // 0: eagle, 1: fruit, 2: pebble, 3: worm
-            int prefabIndex = Random.Range(0, allPrefabs.Length);
-
-            if (prefabIndex == 0) // eagle
-            {
-                y = Random.Range(EAGLE_LIMIT_DOWN, EAGLE_LIMIT_UP);
-                z = Random.Range(EAGLE_LIMIT_LEFT, EAGLE_LIMIT_RIGHT);
-            }
-            if (prefabIndex == 1) // fruit
-            {
-                y = Random.Range(FRUIT_LIMIT_DOWN, FRUIT_LIMIT_UP);
-                z = Random.Range(FRUIT_LIMIT_LEFT, FRUIT_LIMIT_RIGHT);
-            }
-            if (prefabIndex == 2) // pebble
-            {
-                y = PEBBLE_Y;
-                z = Random.Range(PEBBLE_LIMIT_LEFT, PEBBLE_LIMIT_RIGHT);
-            }
-            if (prefabIndex == 3) // worm
-            {
-                y = WORM_Y;
-                z = Random.Range(WORM_LIMIT_LEFT, WORM_LIMIT_RIGHT);
-            }
-
-            Vector3 spawnPos = new Vector3(x, y, z);
-
-            Instantiate(
-                allPrefabs[prefabIndex],
-                spawnPos,
-                allPrefabs[prefabIndex].transform.rotation
-            );
+            y = Random.Range(EAGLE_LIMIT_DOWN, EAGLE_LIMIT_UP);
+            z = Random.Range(EAGLE_LIMIT_LEFT, EAGLE_LIMIT_RIGHT);
         }
+        if (prefabIndex == 1) // fruit
+        {
+            y = Random.Range(FRUIT_LIMIT_DOWN, FRUIT_LIMIT_UP);
+            z = Random.Range(FRUIT_LIMIT_LEFT, FRUIT_LIMIT_RIGHT);
+        }
+        if (prefabIndex == 2) // pebble
+        {
+            y = PEBBLE_Y;
+            z = Random.Range(PEBBLE_LIMIT_LEFT, PEBBLE_LIMIT_RIGHT);
+        }
+        if (prefabIndex == 3) // worm
+        {
+            y = WORM_Y;
+            z = Random.Range(WORM_LIMIT_LEFT, WORM_LIMIT_RIGHT);
+        }
+
+        Vector3 spawnPos = new Vector3(x, y, z);
+
+        Instantiate(allPrefabs[prefabIndex], spawnPos, allPrefabs[prefabIndex].transform.rotation);
     }
 }
