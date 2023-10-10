ee1d3e894e3f44a2057ed57d8bbba7ca05b243ae
pppiyo <yueqinli@usc.edu>
2023-10-09 22:23:16 -0700

    test cases for firebase connection

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class User
+{
+    public string userName;
+    public string userScore;
+
+    // private System.Random random = new System.Random();
+
+    public User()
+    {
+        PlayerScores player = new PlayerScores();
+
+        // userName = PlayerScores.playerName;
+        // userScore = player.scoreText.text;
+    }
+}
