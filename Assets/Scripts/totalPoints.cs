523fa7ee71a614b467fbce575e502c05e497ff1c
pppiyo <yueqinli@usc.edu>
2023-10-09 23:30:12 -0700

    Data analysis 1.0 done. Metric: game start time.

--- Assets/Scripts/totalPoints.cs
+++ Assets/Scripts/totalPoints.cs
@@ -1,16 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Proyecto26;
+
+public class User
+{
+    public string userScore = "0";
+    public string currentTime = DateTime.Now.ToString();
+
+    // public User() {
+    //     totalPoints scores = new totalPoints();
+    //     userScore = socres.points.ToString();
+    //     // userScore = player.scoreText.text;
+    // }
+}
 
 public class totalPoints : MonoBehaviour
 {
     public int points = 0;
     public Text scoreText;
 
+    private User user;
+
     void Start()
     {
+        user = new User();
         UpdateScoreDisplay();
+        RestClient.Post(
+            // "https://mamachick-ff15d-default-rtdb.firebaseio.com/" + playerName + ".json",
+            "https://mamachick-ff15d-default-rtdb.firebaseio.com/.json",
+            user
+        );
     }
 
     //void update()
@@ -31,5 +53,6 @@ public class totalPoints : MonoBehaviour
     {
         Debug.Log("Total Score: " + points.ToString());
         scoreText.text = "Total Score: " + points.ToString();
+        user.userScore = points.ToString();
     }
 }
