352bb4b1bddbc9f2880fd48e57714a18e8568f57
Haoxiang310 <gengh@usc.edu>
2023-10-16 12:54:03 -0700

    Improved newly suggested UI

--- Assets/Scripts/tutorial/TutorialUIManager.cs
+++ Assets/Scripts/tutorial/TutorialUIManager.cs
@@ -13,12 +13,17 @@ public class TutorialUIManager : MonoBehaviour
     [SerializeField] private float arrowYOffset = 2.0f; // New variable for the Y-axis offset
     [SerializeField] private Camera mainCamera;
 
-    // public GameObject eagle;
+    [SerializeField] private GameObject[] arrows = new GameObject[5]; // Reference to the arrow GameObjects
+    [SerializeField] private TMP_Text[] textLabels = new TMP_Text[5]; // Reference to the text labels
+    
+    [SerializeField] private GameObject[] objectsToReferenceInStep1;
 
-    // private float EAGLE_LIMIT_UP = 100;
-    // private float EAGLE_LIMIT_DOWN = 65;
-    // private float EAGLE_LIMIT_LEFT = 300;
-    // private float EAGLE_LIMIT_RIGHT = 100;
+    public GameObject eagle;
+
+    private float EAGLE_LIMIT_UP = 100;
+    private float EAGLE_LIMIT_DOWN = 65;
+    private float EAGLE_LIMIT_LEFT = 300;
+    private float EAGLE_LIMIT_RIGHT = 100;
 
     private int currentBatch = 0;
 
@@ -32,24 +37,53 @@ public class TutorialUIManager : MonoBehaviour
     {
         // Debug.Log(textBatches.Length);
         if (batchIndex < 0 || batchIndex >= textBatches.Length) return;
-
+        
         displayText.text = textBatches[batchIndex];
 
-        // Control arrow visibility and position based on currentBatch
-        if (batchIndex >= 0 && batchIndex < objectsToReference.Length)
+        if (batchIndex == 0)
         {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < objectsToReference.Length)
+                {
+                    arrows[i].SetActive(true);
+                    textLabels[i].gameObject.SetActive(true);
+                    SetArrowAndLabelPosition(arrows[i], textLabels[i], objectsToReferenceInStep1[i].transform.position);
+                }
+                else
+                {
+                    arrows[i].SetActive(false);
+                    textLabels[i].gameObject.SetActive(false);
+                }
+            }
+            displayText.gameObject.SetActive(false); // Hide the upper left text
+        }
+        else
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                arrows[i].SetActive(false);
+                textLabels[i].gameObject.SetActive(false);
+            }
+            displayText.gameObject.SetActive(true); // Show the upper left text
+        }
+        
+        // Control arrow visibility and position based on currentBatch
+        if (batchIndex > 0)
+        {
+            
+            if (batchIndex == 4)
+            {
+                // spawn eagle
+                SpawnRandomEagle();
+            }
+            
             SetArrowPosition(objectsToReference[batchIndex].transform.position);
         }
         else
         {
             arrow.SetActive(false); // Hide the arrow
         }
-
-        // if (batchIndex == 6)
-        // {
-        //     // spawn eagle
-        //     SpawnRandomEagle();
-        // }
 
         // 如果这是最后一批文字，更改按钮的文本
         if (batchIndex == textBatches.Length - 1)
@@ -73,18 +107,19 @@ public class TutorialUIManager : MonoBehaviour
 
     private void EndTutorial()
     {
+        // DestroyImmediate(eagle,true);
         nextButton.gameObject.SetActive(false); // Hide the button
         displayText.gameObject.SetActive(false); // Hide the text
         arrow.SetActive(false); // Hide the arrow
     }
 
-    // private void SpawnRandomEagle()
-    // {
-    //     float y = Random.Range(EAGLE_LIMIT_DOWN, EAGLE_LIMIT_UP);
-    //     float z = Random.Range(EAGLE_LIMIT_LEFT, EAGLE_LIMIT_RIGHT);
-    //     Vector3 spawnPos = new Vector3(0, y, z);
-    //     Instantiate(eagle, spawnPos, eagle.transform.rotation);
-    // }
+    private void SpawnRandomEagle()
+    {
+        float y = Random.Range(EAGLE_LIMIT_DOWN, EAGLE_LIMIT_UP);
+        float z = Random.Range(EAGLE_LIMIT_LEFT, EAGLE_LIMIT_RIGHT);
+        Vector3 spawnPos = new Vector3(0, y, z);
+        Instantiate(eagle, spawnPos, eagle.transform.rotation);
+    }
 
     private void SetArrowPosition(Vector3 referenceObjectPosition)
     {
@@ -98,9 +133,29 @@ public class TutorialUIManager : MonoBehaviour
         RectTransformUtility.ScreenPointToLocalPointInRectangle(arrow.transform.parent.GetComponent<RectTransform>(), screenPosition, mainCamera, out canvasPosition);
     
         // Set the arrow's position to be slightly above the object's position
-        canvasPosition.y += 30;  // 50 is just an example offset, adjust as needed
+        canvasPosition.y += 35;  // 50 is just an example offset, adjust as needed
     
         arrow.GetComponent<RectTransform>().anchoredPosition = canvasPosition;
+    }
+    
+    private void SetArrowAndLabelPosition(GameObject arrow, TMP_Text objectLabel, Vector3 referenceObjectPosition, bool placeLabelOnRight = true)
+    {
+        arrow.SetActive(true);
+    
+        // Convert the 3D object's world position to screen position
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(referenceObjectPosition);
+    
+        // Convert the screen position to canvas position
+        Vector2 canvasPosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(arrow.transform.parent.GetComponent<RectTransform>(), screenPosition, mainCamera, out canvasPosition);
+    
+        // Set the arrow's position to be slightly above the object's position
+        canvasPosition.y += 35;  // Adjust as needed
+        arrow.GetComponent<RectTransform>().anchoredPosition = canvasPosition;
+    
+        // Adjust position for the text label
+        objectLabel.rectTransform.anchoredPosition = new Vector2(canvasPosition.x +objectLabel.rectTransform.sizeDelta.x / 2, canvasPosition.y);
+        
     }
 
 }
