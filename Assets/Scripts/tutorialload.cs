19560dfa700388683404be84b324a4c8127ed03a
nitheshreddy939 <50142613+nitheshreddy939@users.noreply.github.com>
2023-10-17 13:00:43 -0700

    commit

---
+++ Assets/Scripts/tutorialload.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tutorialload : MonoBehaviour
+{
+    public void LoadtutorialScene()
+    {
+        SceneManager.LoadScene("tutorial");
+    }
+}
